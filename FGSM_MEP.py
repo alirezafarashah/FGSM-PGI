b39f9d0d40f04e373b85d7ee9eb0f57ab7799e5d
alirezafarashah <63104907+alirezafarashah@users.noreply.github.com>
2023-11-07 12:52:02 +0330

    Update FGSM_MEP.py

--- FGSM_MEP.py
+++ FGSM_MEP.py
@@ -300,6 +300,7 @@ def main():
 
         epoch_time = time.time()
         lr = scheduler.get_lr()[0]
+        print(train_loss / train_n, train_acc / train_n)
         logger.info('%d \t %.1f \t \t %.4f \t %.4f \t %.4f',
                     epoch, epoch_time - start_epoch_time, lr, train_loss / train_n, train_acc / train_n)
 
